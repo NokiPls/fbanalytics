854d54992b004e7a3fa31c078aa1323bdf0f5b1d
spiritokamikazer <lorenzograziano90@gmail.com>
2014-05-26 12:02:45 +0200

    fix

--- gmfb/src/main/java/gmfb/Graph.java
+++ gmfb/src/main/java/gmfb/Graph.java
@@ -30,39 +30,5 @@ public class Graph {
 
 	
 
-	@RequestMapping(value = "/openGraph", method = RequestMethod.POST)
-	public String friendsCheckboxesSubmit(
-			@RequestParam(value = "id[]", required = false) String[] idSelected,
-			Model model) {
-		if (idSelected.length == 0)
-			return "redirect:/friendsList";
-		if (!facebook.isAuthorized()) {
-			return "redirect:/connect/facebook";
-		}
-
-		// creo Lista di amici selezionati e per ognuno di essi la lista degli
-		// amici in comune
-		for (int i = 0; i < idSelected.length; i++) {
-			PagedList<Reference> mutual = facebook.friendOperations()
-					.getMutualFriends(idSelected[i]);
-
-			FacebookProfile friend = facebook.userOperations().getUserProfile(
-					idSelected[i]);
-			CommonFriendsList.add(i,
-					new Friends(friend.getId(), friend.getName()));
-			for (int k = 0; k < mutual.size(); k++) {
-				CommonFriendsList
-						.get(i)
-						.getCommonFriends()
-						.add(new Friends(mutual.get(k).getId(), mutual.get(k)
-								.getName()));
-
-			}
-		}
-
-		model.addAttribute(facebook.userOperations().getUserProfile());
-		model.addAttribute("Friends", CommonFriendsList);
-		return "hierarchicallist";
-	}
 
 }
