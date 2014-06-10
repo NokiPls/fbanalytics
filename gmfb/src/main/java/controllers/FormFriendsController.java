ce985ccc4fad9b56ab6e645984984d0aecf2403b
Nicola <Nicola@10.168.97.97>
2014-06-10 10:53:47 +0200

    Niente di che

--- gmfb/src/main/java/controllers/FormFriendsController.java
+++ gmfb/src/main/java/controllers/FormFriendsController.java
@@ -26,7 +26,7 @@ public class FormFriendsController {
 
 	private Facebook facebook;
 	public ArrayList<Friend> commonFriendsList = new ArrayList<Friend>();
-	public	SingleGraph graphF = new SingleGraph("graph");
+	public SingleGraph graphF = new SingleGraph("graph");
 
 	@Inject
 	public FormFriendsController(Facebook facebook) {
@@ -39,11 +39,12 @@ public class FormFriendsController {
 		if (!facebook.isAuthorized()) {
 			return "redirect:/connect/facebook";
 		}
-		//the service create list of friends to display in the checkboxes
-		ListOfFriends friends= new ListOfFriends(facebook);
+		// the service create list of friends to display in the checkboxes
+		ListOfFriends friends = new ListOfFriends(facebook);
 
-		model.addAttribute(facebook.userOperations().getUserProfile());
-		model.addAttribute("names", friends.getListOfName()).addAttribute("id", friends.getListOfId());
+		model.addAttribute(facebook.userOperations().getUserProfile())
+				.addAttribute("names", friends.getListOfName())
+				.addAttribute("id", friends.getListOfId());
 		return "friendsList";
 	}
 
@@ -52,16 +53,16 @@ public class FormFriendsController {
 			@RequestParam(value = "id[]", required = false) String[] idSelected,
 			Model model) {
 
-		if (idSelected==null)
+		if (idSelected == null)
 			return "redirect:/List";
 		if (!facebook.isAuthorized()) {
 			return "redirect:/connect/facebook";
 		}
-		
-		// service crea Lista di amici selezionati e per ognuno di essi la lista degli
-		// amici in comune
-		CommonFriendsList common=new CommonFriendsList(facebook,idSelected );
-		commonFriendsList=common.getCommonFriends();
+
+		// service crea Lista di amici selezionati e per ognuno di essi la lista
+		// degli amici in comune
+		CommonFriendsList common = new CommonFriendsList(facebook, idSelected);
+		commonFriendsList = common.getCommonFriends();
 
 		model.addAttribute(facebook.userOperations().getUserProfile());
 		model.addAttribute("Friends", commonFriendsList);
@@ -75,67 +76,71 @@ public class FormFriendsController {
 		}
 
 		CreateJson json = new CreateJson(commonFriendsList, facebook
-				.userOperations().getUserProfile().getName(), Long.parseLong(facebook
-				.userOperations().getUserProfile().getId()));
+				.userOperations().getUserProfile().getName(),
+				Long.parseLong(facebook.userOperations().getUserProfile()
+						.getId()));
 
-		//creo grafo per statistica
+		// creo grafo per statistica
 		String myId = facebook.userOperations().getUserProfile().getId();
-		CreateGraph graph=new CreateGraph(commonFriendsList, myId);
-		graphF=graph.getGraphF();
-		/* compute all the metrics*/
+		CreateGraph graph = new CreateGraph(commonFriendsList, myId);
+		graphF = graph.getGraphF();
+		/* compute all the metrics */
 
-		//compute DegreeCentrality centrality for each node
-		DegreeCentrality dc=new	DegreeCentrality();
+		// compute DegreeCentrality centrality for each node
+		DegreeCentrality dc = new DegreeCentrality();
 		dc.init(graphF);
 		dc.setCentralityAttribute("degree");
 		dc.compute();
-		
-		//compute normalized DegreeCentrality centrality for each node
-		DegreeCentrality ndc=new DegreeCentrality("norm_degree", AbstractCentrality.NormalizationMode.SUM_IS_1);
+
+		// compute normalized DegreeCentrality centrality for each node
+		DegreeCentrality ndc = new DegreeCentrality("norm_degree",
+				AbstractCentrality.NormalizationMode.SUM_IS_1);
 		ndc.init(graphF);
 		ndc.compute();
-	
-		//compute betweenness centrality for each node
-			BetweennessCentrality bc = new BetweennessCentrality("betweenness");
-	        bc.init(graphF);
-	        bc.compute();
-	
-	      //compute ClosenessCentrality centrality for each node
-	        ClosenessCentrality cc= new ClosenessCentrality("closeness");
-	        cc.init(graphF);
-	        cc.compute();
-	        
-	      //compute normalized ClosenessCentrality centrality for each node
-	        ClosenessCentrality ncc= new ClosenessCentrality("norm_closeness", AbstractCentrality.NormalizationMode.SUM_IS_1, true, false);
-	        ncc.init(graphF);
-	        ncc.compute();
-	      
-	        
+
+		// compute betweenness centrality for each node
+		BetweennessCentrality bc = new BetweennessCentrality("betweenness");
+		bc.init(graphF);
+		bc.compute();
+
+		// compute ClosenessCentrality centrality for each node
+		ClosenessCentrality cc = new ClosenessCentrality("closeness");
+		cc.init(graphF);
+		cc.compute();
+
+		// compute normalized ClosenessCentrality centrality for each node
+		ClosenessCentrality ncc = new ClosenessCentrality("norm_closeness",
+				AbstractCentrality.NormalizationMode.SUM_IS_1, true, false);
+		ncc.init(graphF);
+		ncc.compute();
+
 		model.addAttribute("graph", json.getJson());
 		model.addAttribute(facebook.userOperations().getUserProfile());
 		model.addAttribute("Friends", commonFriendsList);
 
 		return "graphPage";
 	}
-	
-	
+
 	@RequestMapping(value = "/GraphNode", method = RequestMethod.GET)
-	public String GraphNode(Model model,@RequestParam(value="id")  String id) {
+	public String GraphNode(Model model, @RequestParam(value = "id") String id) {
 		if (!facebook.isAuthorized()) {
 			return "redirect:/connect/facebook";
 		}
-		
-		
+
 		FacebookProfile profile = facebook.userOperations().getUserProfile(id);
 
-		model.addAttribute("betweenness", graphF.getNode(id).getAttribute("betweenness"));
-		model.addAttribute("closeness", graphF.getNode(id).getAttribute("closeness"));
-		model.addAttribute("norm_closeness", graphF.getNode(id).getAttribute("norm_closeness"));
+		model.addAttribute("betweenness",
+				graphF.getNode(id).getAttribute("betweenness"));
+		model.addAttribute("closeness",
+				graphF.getNode(id).getAttribute("closeness"));
+		model.addAttribute("norm_closeness",
+				graphF.getNode(id).getAttribute("norm_closeness"));
 		model.addAttribute("degree", graphF.getNode(id).getAttribute("degree"));
-		model.addAttribute("norm_degree", graphF.getNode(id).getAttribute("norm_degree"));
-		
+		model.addAttribute("norm_degree",
+				graphF.getNode(id).getAttribute("norm_degree"));
+
 		model.addAttribute("profile", profile);
 		return "nodePopUp";
 	}
-	
+
 }
