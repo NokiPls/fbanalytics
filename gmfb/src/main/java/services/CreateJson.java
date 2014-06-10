2e71f50363694e85a7a73b7da043cb50496247a2
Nicola <Nicola@10.168.97.97>
2014-06-10 10:29:07 +0200

    Final stuff in createjson

--- gmfb/src/main/java/services/CreateJson.java
+++ gmfb/src/main/java/services/CreateJson.java
@@ -6,36 +6,42 @@ import bean.Friend;
 
 public class CreateJson {
 	private String json, links = "";
+	private final String NODES = "\"nodes\":";
+	private final String NAME = "\"name\":";
+	private final String ID = "\"id\":";
+	private final String LINKS = "\"links\":";
+	private final String SOURCE = "\"source\":";
+	private final String TARGET = "\"target\":";
+
 	// The json is like
 	// {"nodes":[{"name":"John Bonham","group":1},{..}],"links":[{"source":"int","target":"int","value":"int"}]}
 	// in which source and target are the position in the array "nodes" of the
 	// elements to be linked
-	public CreateJson(ArrayList<Friend> friend, String self, Long id) {
+	public CreateJson(ArrayList<Friend> friend, String self, Long fid) {
 		int j = 0;
 		int i = 0;
 		ArrayList<Long> idPos = new ArrayList<Long>();
 		// Adding the user as position 0
-		idPos.add(0, id);
-		if(self.contains("\'"))
-			self=self.replaceAll("\'", "`");
-		json = "{\"nodes\":[{\"name\":\"" + self + "\",\"id\":\"" + id
-				+ "\"},";
+		idPos.add(0, fid);
+		if (self.contains("\'"))
+			self = self.replaceAll("\'", "`");
+		json = "{" + NODES + "[{" + NAME + "\"" + self + "\"," + ID + "\""
+				+ fid + "\"},";
 		for (i = 0; i < friend.size(); i++) {
 			// "i" are the selected friends
 			if (!idPos.contains(friend.get(i).getId())) {
 				idPos.add(friend.get(i).getId());
 				String nameA = friend.get(i).getName();
-				if(nameA.contains("\'"))
-					nameA=nameA.replaceAll("\'", "`");
-				json += "{\"name\":\"" + nameA
-						+ "\",\"id\":\"" + friend.get(i).getId()
-						+ "\"},";
+				if (nameA.contains("\'"))
+					nameA = nameA.replaceAll("\'", "`");
+				json += "{" + NAME + "\"" + nameA + "\"," + ID + "\""
+						+ friend.get(i).getId() + "\"},";
 				// fpos keeps track of the position of the direct friend in the
 				// json
 				// link them to the user
-				links += "{\"source\":0,\"target\":"
+				links += "{" + SOURCE + "0," + TARGET + ""
 						+ idPos.indexOf(friend.get(i).getId())
-						+ ",\"value\":1},";
+						+ "},";
 			}
 			ArrayList<Friend> common = friend.get(i).getCommonFriends();
 			for (j = 0; j < common.size(); j++) {
@@ -43,34 +49,33 @@ public class CreateJson {
 					idPos.add(common.get(j).getId());
 					// "j" are the common friends between me and "i"
 					String nameB = common.get(j).getName();
-					if(nameB.contains("'"))
-						nameB=nameB.replace("'", "`");
-					json += "{\"name\":\"" + nameB
-							+ "\",\"id\":\"" + common.get(j).getId()
-							+ "\"},";
+					if (nameB.contains("'"))
+						nameB = nameB.replace("'", "`");
+					json += "{" + NAME + "\"" + nameB + "\"," + ID + "\""
+							+ common.get(j).getId() + "\"},";
 					// link them to the user
-					links += "{\"source\":0,\"target\":"
+					links += "{" + SOURCE + "0," + TARGET + ""
 							+ idPos.indexOf(common.get(j).getId())
-							+ ",\"value\":1},";
+							+ "},";
 					// link them to the "i" friend
-					links += "{\"source\":"
-							+ idPos.indexOf(common.get(j).getId())
-							+ ",\"target\":"
+					links += "{" + SOURCE + ""
+							+ idPos.indexOf(common.get(j).getId()) + ","
+							+ TARGET + ""
 							+ idPos.indexOf(friend.get(i).getId())
-							+ ",\"value\":1},";
+							+ "},";
 				} else {
-					links += "{\"source\":"
-							+ idPos.indexOf(common.get(j).getId())
-							+ ",\"target\":"
+					links += "{" + SOURCE + ""
+							+ idPos.indexOf(common.get(j).getId()) + ","
+							+ TARGET + ""
 							+ idPos.indexOf(friend.get(i).getId())
-							+ ",\"value\":1},";
+							+ "},";
 				}
 			}
 		}
 		// finalize the json
 		links = (String) links.subSequence(0, links.length() - 1);
 		json = (String) json.subSequence(0, json.length() - 1);
-		json += "],\"links\":[" + links + "]}";
+		json += "]," + LINKS + "[" + links + "]}";
 	}
 
 	public String getJson() {
