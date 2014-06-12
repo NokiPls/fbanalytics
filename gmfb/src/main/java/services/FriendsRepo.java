ecef5e6e30a6a7604f9b883664cd2b9cb237d718
Nicola <Nicola@Nicola-PC>
2014-06-12 21:30:11 +0200

    Transactional, mannaggia a me

--- gmfb/src/main/java/services/FriendsRepo.java
+++ gmfb/src/main/java/services/FriendsRepo.java
@@ -1,6 +1,7 @@
 package services;
 
 import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -11,9 +12,11 @@ import bean.Friend;
 @Service
 @Repository
 public class FriendsRepo{
+	
 	@Autowired
 	private EntityManager em;
 	
+	@Transactional
 	public void addFriend(Friend friend) {
 		//friend è l'user, quello che fa la query
 	em.persist(friend);;
