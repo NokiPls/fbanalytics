9358e7ffa5eae14c07c8b10b6dd678434be7ecdf
Nicola <Nicola@10.168.97.181>
2014-06-23 15:37:20 +0200

    Added graph attributes in friend class

--- gmfb/src/main/java/domain/Friend.java
+++ gmfb/src/main/java/domain/Friend.java
@@ -19,7 +19,15 @@ public class Friend implements Serializable {
 
 	private static final long serialVersionUID = 8828388186922307614L;
 	private String name;
+	//To separate persons which are friends with different app users
 	private Long appUserId;
+	//To distinguish between same person, same app user, but different graph requests
+	private int graphNumber;
+	private int degreeCentrality;
+	private int normalizedDegreeCentrality;
+	private int betweennessCentrality;
+	private int closenessCentrality;
+	private int normalizedClosenessCentrality;
 	private Long id;
 
 	@Id
@@ -36,7 +44,7 @@ public class Friend implements Serializable {
 	};
 
 	public Friend(Long newId, String newName, Friend parent, Long appUserId) {
-		this.appUserId = appUserId;
+		this.setAppUserId(appUserId);
 		this.id = newId;
 		this.name = newName;
 		this.parent = parent;
@@ -68,6 +76,63 @@ public class Friend implements Serializable {
 
 	public void setParent(Friend parent) {
 		this.parent = parent;
+	}
+
+	public int getGraphNumber() {
+		return graphNumber;
+	}
+
+	public void setGraphNumber(int graphNumber) {
+		this.graphNumber = graphNumber;
+	}
+
+	public Long getAppUserId() {
+		return appUserId;
+	}
+
+	public void setAppUserId(Long appUserId) {
+		this.appUserId = appUserId;
+	}
+
+	public int getDegreeCentrality() {
+		return degreeCentrality;
+	}
+
+	public void setDegreeCentrality(int degreeCentrality) {
+		this.degreeCentrality = degreeCentrality;
+	}
+
+	public int getNormalizedDegreeCentrality() {
+		return normalizedDegreeCentrality;
+	}
+
+	public void setNormalizedDegreeCentrality(int normalizedDegreeCentrality) {
+		this.normalizedDegreeCentrality = normalizedDegreeCentrality;
+	}
+
+	public int getBetweennessCentrality() {
+		return betweennessCentrality;
+	}
+
+	public void setBetweennessCentrality(int betweennessCentrality) {
+		this.betweennessCentrality = betweennessCentrality;
+	}
+
+	public int getClosenessCentrality() {
+		return closenessCentrality;
+	}
+
+	public void setClosenessCentrality(int closenessCentrality) {
+		this.closenessCentrality = closenessCentrality;
+	}
+
+	public int getNormalizedClosenessCentrality() {
+		return normalizedClosenessCentrality;
+	}
+
+	public void setNormalizedClosenessCentrality(
+			int normalizedClosenessCentrality) {
+		this.normalizedClosenessCentrality = normalizedClosenessCentrality;
 	}
 
 }
--- gmfb/src/main/java/services/Graph.java
+++ gmfb/src/main/java/services/Graph.java
@@ -1,6 +1,5 @@
 package services;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.graphstream.algorithm.BetweennessCentrality;
