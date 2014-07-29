7eef5a6522518e3cc95829c62809a4f66366ee34
Nokipls <nokimrn@gmail.com>
2014-07-29 12:42:23 +0200

    CreateJson rewritten

--- gmfb/src/main/java/com/lgnm/fb/services/CreateJson.java
+++ gmfb/src/main/java/com/lgnm/fb/services/CreateJson.java
@@ -3,54 +3,82 @@ package com.lgnm.fb.services;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.stereotype.Service;
 
 import com.lgnm.fb.domain.Friend;
 
 @Service
 public class CreateJson implements CreateJsonInterface {
-	private String json, links = "";
-	private final String NODES = "\"nodes\":[";
-	private final String NAME = "\"name\":";
-	private final String ID = "\"id\":";
-	private final String LINKS = "\"links\":[";
-	private final String SOURCE = "\"source\":";
-	private final String TARGET = "\"target\":";
+	private String json = "";
+	private final String NODES = "nodes";
+	private final String NAME = "name";
+	private final String ID = "id";
+	private final String LINKS = "links";
+	private final String SOURCE = "source";
+	private final String TARGET = "target";
 
 	public CreateJson() {
 	}
 
 	// The json is like
-	// {"nodes":[{"name":"John Bonham","group":1},{..}],"links":[{"source":"int","target":"int","value":"int"}]}
+	// {"nodes":[{"name":"John Bonham","group":1},{..}],"links":[{"source": int,"target": int,"value": int}]}
 	// in which source and target are the position in the array "nodes" of the
 	// elements to be linked
 	@Override
 	public void makeJson(List<Friend> friend, String self, Long fid) {
-		json= "";
-		links = "";
+		json = "";
 		int j = 0;
 		int i = 0;
+
+		JSONObject mainJson = new JSONObject();
+		JSONArray nodes = new JSONArray();
+		JSONObject node = new JSONObject();
+		JSONArray links = new JSONArray();
+		JSONObject link = new JSONObject();
+
 		ArrayList<Long> idPos = new ArrayList<Long>();
 		// Adding the user as position 0
 		idPos.add(0, fid);
-		if (self.contains("\'"))
+		if (self.contains("\'")) {
 			self = self.replaceAll("\'", "`");
-		json = "{" + NODES + "{" + NAME + "\"" + self + "\"," + ID + "\"" + fid
-				+ "\"},";
+		}
+		
+		try {
+			node.put(NAME, self).put(ID, fid.toString());
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		nodes.put(node);
+		node = new JSONObject();
+
 		for (i = 0; i < friend.size(); i++) {
 			// "i" are the selected friends
 			if (!idPos.contains(friend.get(i).getFbId())) {
 				idPos.add(friend.get(i).getFbId());
 				String nameA = friend.get(i).getName();
-				if (nameA.contains("\'"))
+				if (nameA.contains("\'")) {
 					nameA = nameA.replaceAll("\'", "`");
-				json += "{" + NAME + "\"" + nameA + "\"," + ID + "\""
-						+ friend.get(i).getFbId() + "\"},";
+				}
+		
+				try {
+					node.put(NAME, nameA).put(ID,
+							friend.get(i).getFbId().toString());
+					nodes.put(node);
+					link.put(SOURCE, 0).put(TARGET,
+							idPos.indexOf(friend.get(i).getFbId()));
+					links.put(link);
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+
 				// fpos keeps track of the position of the direct friend in the
 				// json
 				// link them to the user
-				links += "{" + SOURCE + "0," + TARGET + ""
-						+ idPos.indexOf(friend.get(i).getFbId()) + "},";
+				node = new JSONObject();
+				link = new JSONObject();
 			}
 			List<Friend> common = friend.get(i).getCommonFriends();
 			for (j = 0; j < common.size(); j++) {
@@ -58,30 +86,59 @@ public class CreateJson implements CreateJsonInterface {
 					idPos.add(common.get(j).getFbId());
 					// "j" are the common friends between me and "i"
 					String nameB = common.get(j).getName();
-					if (nameB.contains("'"))
+					if (nameB.contains("'")) {
 						nameB = nameB.replace("'", "`");
-					json += "{" + NAME + "\"" + nameB + "\"," + ID + "\""
-							+ common.get(j).getFbId() + "\"},";
-					// link them to the user
-					links += "{" + SOURCE + "0," + TARGET + ""
-							+ idPos.indexOf(common.get(j).getFbId()) + "},";
+					}
+					
+					try {
+						node.put(NAME, nameB).put(ID,
+								common.get(j).getFbId().toString());
+						nodes.put(node);
+						// link them to the user
+						link.put(SOURCE, 0).put(TARGET,
+								idPos.indexOf(common.get(j).getFbId()));
+						links.put(link);
+					} catch (JSONException e) {
+						e.printStackTrace();
+					}
+	
+					node = new JSONObject();
+					link = new JSONObject();
+
 					// link them to the "i" friend
-					links += "{" + SOURCE + ""
-							+ idPos.indexOf(common.get(j).getFbId()) + ","
-							+ TARGET + ""
-							+ idPos.indexOf(friend.get(i).getFbId()) + "},";
+					try {
+						link.put(SOURCE, idPos.indexOf(common.get(j).getFbId()))
+								.put(TARGET,
+										idPos.indexOf(friend.get(i).getFbId()));
+						links.put(link);
+					} catch (JSONException e) {
+						e.printStackTrace();
+					}
+					link = new JSONObject();
+
 				} else {
-					links += "{" + SOURCE + ""
-							+ idPos.indexOf(common.get(j).getFbId()) + ","
-							+ TARGET + ""
-							+ idPos.indexOf(friend.get(i).getFbId()) + "},";
+					try {
+						link.put(SOURCE, idPos.indexOf(common.get(j).getFbId()))
+								.put(TARGET,
+										idPos.indexOf(friend.get(i).getFbId()));
+						links.put(link);
+					} catch (JSONException e) {
+						e.printStackTrace();
+					}
+					link = new JSONObject();
 				}
 			}
 		}
 		// finalize the json
-		links = (String) links.subSequence(0, links.length() - 1);
-		json = (String) json.subSequence(0, json.length() - 1);
-		json += "]," + LINKS + links + "]}";
+	
+		try {
+			mainJson.put(NODES, nodes).put(LINKS, links);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+
+		json = mainJson.toString();
+
 	}
 
 	@Override
--- gmfb/src/main/java/com/lgnm/fb/services/CreateJsonInterface.java
+++ gmfb/src/main/java/com/lgnm/fb/services/CreateJsonInterface.java
@@ -1,11 +1,11 @@
 package com.lgnm.fb.services;
 
 import java.util.List;
-
 import com.lgnm.fb.domain.Friend;
 
 public interface CreateJsonInterface {
 
 	public void makeJson(List<Friend> friend, String self, Long fid);
+
 	public String getJson();
 }
